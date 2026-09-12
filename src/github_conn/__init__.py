03192d394caa8d288236211dbd06c06d383cc1fc
Alessandro <317975992+Dal-Canto@users.noreply.github.com>
2026-09-12 16:54:59 +0200

    Update __init__.py

--- src/github_conn/__init__.py
+++ src/github_conn/__init__.py
@@ -1,3 +1,7 @@
+#  -*- coding: utf-8 -*-
+#  github_conn — Copyright (c) 2024-2026 Alessandro Dal-Canto
+#  Rilasciato sotto i termini della licenza MIT.
+
 from .client import GitHubClient
 from .exceptions import (
     GitHubConnException,
